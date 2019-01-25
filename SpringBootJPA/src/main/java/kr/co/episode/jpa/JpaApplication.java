79e62a948e649930b627cdb8f0a5d05a153e8995
skyer9 <skyer9@gmail.com>
2019-01-26 08:23:01 +0900

    JPA 샘플코드

--- SpringBootJPA/src/main/java/kr/co/episode/jpa/JpaApplication.java
+++ SpringBootJPA/src/main/java/kr/co/episode/jpa/JpaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpaApplication {
+    // from http://www.libqa.com/wiki/730
     public static void main(String[] args) {
         SpringApplication.run(JpaApplication.class, args);
     }
